0d52c5f7c617e17374eac59ed8ba9e178896f366
Tim Scaffidi <timothy.scaffidi@gmail.com>
2012-11-28 17:18:42 -0800

    more curl functionality

--- src/ofxSSL.cpp
+++ src/ofxSSL.cpp
@@ -13,13 +13,13 @@
 
 //--------------------------------------------------------------
 ofxSSL::ofxSSL() {
-    ofAddListener(ofEvents().setup, this, &ofxSSL::setup);
+//    ofAddListener(ofEvents().setup, this, &ofxSSL::setup);
 }
 
 //--------------------------------------------------------------
 ofxSSL::~ofxSSL() {
     cleanup();
-	ofRemoveListener(ofEvents().setup, this, &ofxSSL::setup);
+//	ofRemoveListener(ofEvents().setup, this, &ofxSSL::setup);
 }
 
 //--------------------------------------------------------------
@@ -28,12 +28,33 @@ void ofxSSL::setup(ofEventArgs &e) {
     handle = curl_easy_init();
     post = NULL;
     last = NULL;
+    header_list = NULL;
+    uploadFile = NULL;
     curl_easy_setopt(handle, CURLOPT_NOPROGRESS, 0);
     curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, true);
     curl_easy_setopt(handle, CURLOPT_VERBOSE, true);
     curl_easy_setopt(handle, CURLOPT_CAINFO, ofToDataPath("cacert.pem").c_str());
-    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writer);
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, content_writer);
     curl_easy_setopt(handle, CURLOPT_WRITEDATA, &content);
+    curl_easy_setopt(handle, CURLOPT_HEADERFUNCTION, header_writer);
+    curl_easy_setopt(handle, CURLOPT_HEADERDATA, &header);
+}
+
+void ofxSSL::setup(){
+    curl_global_init(CURL_GLOBAL_ALL);
+    handle = curl_easy_init();
+    post = NULL;
+    last = NULL;
+    header_list = NULL;
+    uploadFile = NULL;
+    curl_easy_setopt(handle, CURLOPT_NOPROGRESS, 0);
+    curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, true);
+    curl_easy_setopt(handle, CURLOPT_VERBOSE, true);
+    curl_easy_setopt(handle, CURLOPT_CAINFO, ofToDataPath("cacert.pem").c_str());
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, content_writer);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, &content);
+    curl_easy_setopt(handle, CURLOPT_HEADERFUNCTION, header_writer);
+    curl_easy_setopt(handle, CURLOPT_WRITEHEADER, &header);
 }
 
 //--------------------------------------------------------------
@@ -53,6 +74,14 @@ void ofxSSL::setOpt(CURLoption option, int value) {
     curl_easy_setopt(handle, option, value);
 }
 
+void ofxSSL::setUploadFile(string filename) {
+    
+    uploadFile = fopen(ofFilePath::getAbsolutePath(filename).c_str(), "rb");
+    curl_easy_setopt(handle, CURLOPT_UPLOAD, 1);
+    curl_easy_setopt(handle, CURLOPT_READDATA, uploadFile);
+
+}
+
 //--------------------------------------------------------------
 void ofxSSL::addFormField(const string& fieldName, const string& value) {
     
@@ -65,35 +94,83 @@ void ofxSSL::addFormField(CURLformoption optionA, const string& valueA, CURLform
     curl_formadd(&post, &last, optionA, valueA.c_str(), optionB, valueB.c_str());
 }
 
+void ofxSSL::addHeader(string header_line){
+    
+    header_list = curl_slist_append(header_list, header_line.c_str());
+}
+
 //--------------------------------------------------------------
 void ofxSSL::perform() {
     
     if(post != NULL) curl_easy_setopt(handle, CURLOPT_HTTPPOST, post);
+    if(header_list != NULL) curl_easy_setopt(handle, CURLOPT_HTTPHEADER, header_list);
     
     ret = curl_easy_perform(handle);
     if(CURLE_OK != ret) {
         ofLogError("ofxSSL") << curl_easy_strerror(ret);
     }
     
-    if(post != NULL) curl_formfree(post);    
+    if(post != NULL) {
+        curl_formfree(post);
+        post = NULL;
+    }
+    if (header_list != NULL) {
+        curl_slist_free_all(header_list);
+        header_list = NULL;
+    }
+    if(uploadFile != NULL) {
+        fclose(uploadFile);
+        uploadFile = NULL;
+    }
 }
 
 //--------------------------------------------------------------
 void ofxSSL::cleanup() {
-    curl_easy_cleanup(handle);
-    if(post != NULL) curl_formfree(post); 
+    
+    if(handle != NULL) {
+        curl_easy_cleanup(handle);
+        handle = NULL;
+    }
+    if(post != NULL) {
+        curl_formfree(post);
+        post = NULL;
+    }
+    if (header_list != NULL) {
+        curl_slist_free_all(header_list);
+        header_list = NULL;
+    }
+    if(uploadFile != NULL) {
+        fclose(uploadFile);
+        uploadFile = NULL;
+    }
+    content = "";
+    header = "";
 }
 
 //--------------------------------------------------------------
-string ofxSSL::getString() const {
+string ofxSSL::getResponseHeader() const {
+    return header;
+}
+
+//--------------------------------------------------------------
+string ofxSSL::getResponseBody() const {
     return content;
 }
 
 //--------------------------------------------------------------
-size_t ofxSSL::writer(char *ptr, size_t size, size_t nmemb, string userdata) {
+size_t ofxSSL::content_writer(char *ptr, size_t size, size_t nmemb, string userdata) {
         
     string content(static_cast<const char*>(ptr), size * nmemb);
     userdata = content;
         
+    return size * nmemb;
+}
+
+//--------------------------------------------------------------
+size_t ofxSSL::header_writer(char *ptr, size_t size, size_t nmemb, string userdata) {
+    
+    string header(static_cast<const char*>(ptr), size * nmemb);
+    userdata += header;
+    
     return size * nmemb;
 }
--- src/ofxSSL.h
+++ src/ofxSSL.h
@@ -21,31 +21,38 @@ public:
     
     //methods
     void setup(ofEventArgs &e);
+    void setup();
     void setURL(const string& url);
     void setOpt(CURLoption option, const string& value);
     void setOpt(CURLoption option, int value);
+    void setUploadFile(string filename);
     void addFormField(const string& fieldName, const string& value);
     void addFormField(CURLformoption optionA, const string& valueA, CURLformoption optionB, const string& valueB);
+    void addHeader(string header_line);
     void perform();
     
-    string getString() const;
+    string getResponseHeader() const;
+    string getResponseBody() const;
 
+    void cleanup();
 protected:
     //variables
     CURL* handle;
 
-    void cleanup();
     
 private:
     
     //methods
-    static size_t writer(char *ptr, size_t size, size_t nmemb, string userdata);
+    static size_t content_writer(char *ptr, size_t size, size_t nmemb, string userdata);
+    static size_t header_writer(char *ptr, size_t size, size_t nmemb, string userdata);
     
     //variables
     struct curl_httppost *post;
     struct curl_httppost *last;
+    struct curl_slist *header_list;
     CURLcode ret;
     string content;
-    
+    string header;
+    FILE * uploadFile;
 };
 
