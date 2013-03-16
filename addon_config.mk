eafbdee9b09e65d289c84632409aeb58ebd6ed1b
Christopher Baker <me@christopherbaker.net>
2013-03-16 13:55:45 -0500

    Makefile config updates.

--- addon_config.mk
+++ addon_config.mk
@@ -6,6 +6,51 @@ meta:
 	ADDON_URL = https://github.com/thorsonmscott/ofxSSL
 
 common:
+
+linux64:
 	# linux only, any library that should be included in the project using
 	# pkg-config
 	ADDON_PKG_CONFIG_LIBRARIES = libcurl
+
+	# this forces us to use pkg-config headers in the addon
+	ADDON_INCLUDES_EXCLUDE = libs
+	ADDON_INCLUDES_EXCLUDE += libs/%
+	
+linux:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libcurl
+	
+	# this forces us to use pkg-config headers in the addon
+	ADDON_INCLUDES_EXCLUDE = libs
+	ADDON_INCLUDES_EXCLUDE += libs/%
+
+win_cb:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libcurl
+	
+	# this forces us to use pkg-config headers in the addon
+	ADDON_INCLUDES_EXCLUDE = libs
+	ADDON_INCLUDES_EXCLUDE += libs/%
+
+linuxarmv6l:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libcurl
+	
+	# this forces us to use pkg-config headers in the addon
+	ADDON_INCLUDES_EXCLUDE = libs
+	ADDON_INCLUDES_EXCLUDE += libs/%
+linuxarmv7l:
+	# linux only, any library that should be included in the project using
+	# pkg-config
+	ADDON_PKG_CONFIG_LIBRARIES = libcurl
+
+	# this forces us to use pkg-config headers in the addon
+	ADDON_INCLUDES_EXCLUDE = libs
+	ADDON_INCLUDES_EXCLUDE += libs/%
+
+android/armeabi:	
+	
+android/armeabi-v7a:
