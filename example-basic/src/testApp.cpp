3ee8c8fca726b79b806ce43710298743223100d4
Christopher Baker <me@christopherbaker.net>
2013-03-16 11:26:01 -0500

    Cleaned up example.

--- example-basic/src/testApp.cpp
+++ example-basic/src/testApp.cpp
@@ -6,9 +6,6 @@ void testApp::setup(){
 }
 
 //--------------------------------------------------------------
-void testApp::update(){}
-
-//--------------------------------------------------------------
 void testApp::draw(){
     ofDrawBitmapString(result, 10, 10);
 }
@@ -20,63 +17,23 @@ void testApp::keyPressed(int key){
         case '1':
             curl.setURL("http://www.openframeworks.cc");
             curl.perform();
-            result = curl.getString();
+            result = curl.getResponseBody();
             break;
             
         case '2':
             curl.setURL("http://www.ofxaddons.com");
             curl.perform();
-            result = curl.getString();
+            result = curl.getResponseBody();
             break;
             
         case '3':
             curl.setURL("https://test.authorize.net/");
             curl.perform();
-            result = curl.getString();
+            result = curl.getResponseBody();
             break;
             
         default:
             break;
     }
-    
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-    
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
     
 }
